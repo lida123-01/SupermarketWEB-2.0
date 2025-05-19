c68c5d40f57a05cc8a019c3017cc65f885da738f
lida123-01 <lidapinzon06@hotmail.com>
2025-05-18 21:12:30 -0500

    Instalacion de dependencias y creacion del modelo Product

--- obj/Release/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Release/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b32805c1469e74e85ab060a48668a1f1977eb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
