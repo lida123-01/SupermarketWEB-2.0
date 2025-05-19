46eadbbf3e529fe34514f70105ae68c04bd1354f
lida123-01 <lidapinzon06@hotmail.com>
2025-05-18 21:13:56 -0500

    creacion del modelo Category

--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b32805c1469e74e85ab060a48668a1f1977eb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c68c5d40f57a05cc8a019c3017cc65f885da738f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
