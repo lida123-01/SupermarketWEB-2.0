95413fea75191378256220cd574f1c21cd0d1469
lida123-01 <lidapinzon06@hotmail.com>
2025-05-21 23:06:13 -0500

    Index Users

---
+++ Pages/Users/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Users
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public IndexModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> Users { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Users != null)
+            {
+                Users = await _context.Users.ToListAsync();
+            }
+        }
+    }
+}
--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e51601770c14242605c59b2806d11f7cd7b9a47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb12850f259e6b96c937e48c6b909bdd7637b7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
