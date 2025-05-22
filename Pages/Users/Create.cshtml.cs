615964abbde726732e449f23a94d110d1f063d61
lida123-01 <lidapinzon06@hotmail.com>
2025-05-21 23:09:43 -0500

    Create Users

---
+++ Pages/Users/Create.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Users
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public new User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Users == null || User == null)
+            {
+                return Page();
+            }
+
+            _context.Users.Add(User);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./index");
+        }
+    }
+}
